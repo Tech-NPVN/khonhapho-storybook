504778b0020bee75505fb242d056da7bab5ea87b
Pham Tuan <phamanhtuan2833@gmail.com>
2024-05-07 17:39:22 +0700

    feat(components): :sparkles: add `Breadcrumb` component

---
+++ src/components/Navigation/Breadcrumb/index.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Breadcrumb, HomeIcon, ItemBreadcrumb } from '.';
+
+const breadcrumbs: ItemBreadcrumb[] = [
+  { label: 'Home', href: '/', icon: <HomeIcon /> },
+  { label: 'Docs', href: '/docs' },
+  { label: 'Breadcrumbs', href: '/breadcrumbs' },
+];
+
+const meta: Meta<typeof Breadcrumb> = {
+  title: 'Navigation/Breadcrumb',
+  component: Breadcrumb,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {},
+  argTypes: {},
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    breadcrumbs: breadcrumbs,
+  },
+};
