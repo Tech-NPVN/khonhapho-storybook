2017c4e07e14c2c06977f785efc5b4e5923d6936
bou2201 <lamchuc123456@gmail.com>
2024-05-07 17:43:20 +0700

    feat(components): :sparkles: add `Tabs` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Tabs } from './components/DataDisplay';
+
 const App = () => {
-  return <></>;
+  return (
+    <Tabs
+      variantTab="fillGray"
+      tabs={[
+        {
+          name: 'Tab 1',
+          key: 'a',
+          children: <>AAAA</>,
+        },
+        {
+          name: 'Tab 2',
+          key: 'b',
+          children: <>BBBB</>,
+        },
+        {
+          name: 'Tab 3',
+          key: 'c',
+          children: <>C</>,
+        },
+        {
+          name: 'Tab 4',
+          key: 'd',
+          children: <>D</>,
+        },
+      ]}
+    />
+  );
 };
 
 export default App;
--- src/components/DataDisplay/Tabs/index.tsx
+++ src/components/DataDisplay/Tabs/index.tsx
@@ -1,40 +1,78 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
-import clsx from 'clsx';
-import { Fragment } from 'react';
+import { cn } from '@/utils';
+import { Tab, TabGroup, TabList, TabPanels } from '@headlessui/react';
+import { VariantProps, cva } from 'class-variance-authority';
+import { Fragment, ReactNode, useMemo, useState } from 'react';
 
-type TabsProps = {};
+const buttonTabStyles = cva(
+  'py-2 cursor-pointer rounded-lg outline-none transition-colors duration-300 ease-in-out',
+  {
+    variants: {
+      variant: {
+        underline: 'border-b-2 border-transparent hover:text-green rounded-none',
+        underlineSelected: 'border-b-2 border-green text-green rounded-none',
+        fillGray: 'px-[56px]',
+        fillGraySelected: 'px-[56px] bg-[#F3F4F6]',
+        fillGreen: 'px-[56px]',
+        fillGreenSelected: 'px-[56px] bg-green text-white',
+      },
+    },
+    defaultVariants: {
+      variant: 'underline',
+    },
+  },
+);
 
-export const Tabs = () => {
+export type TabRenderProps = {
+  name: string | ReactNode;
+  key: string;
+  children: ReactNode;
+  disabled?: boolean;
+};
+
+export type TabsProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonTabStyles> & {
+    tabs: TabRenderProps[];
+    variantTab?: 'underline' | 'fillGray' | 'fillGreen';
+  };
+
+export const Tabs = ({ tabs, variantTab = 'underline', ...buttonProps }: TabsProps) => {
+  const [tabIndex, setTabIndex] = useState<number>(0);
+
+  const { variant, className, ...props } = buttonProps;
+
+  const getTabListCn = useMemo(() => {
+    switch (variantTab) {
+      case 'underline':
+        return 'border-b border-gray-200 gap-8 px-4';
+      case 'fillGray':
+        return 'bg-[#E5E6E8] p-2 rounded-lg';
+      case 'fillGreen':
+        return 'bg-white p-2 rounded-lg';
+    }
+  }, [variantTab]);
+
   return (
-    <TabGroup manual>
-      <TabList>
-        <Tab as={Fragment}>
-          {({ hover, selected }) => (
-            <button className={clsx(hover && 'underline', selected && 'bg-blue-500 text-white')}>
-              Tab 1
-            </button>
-          )}
-        </Tab>
-        <Tab as={Fragment}>
-          {({ hover, selected }) => (
-            <button className={clsx(hover && 'underline', selected && 'bg-blue-500 text-white')}>
-              Tab 2
-            </button>
-          )}
-        </Tab>
-        <Tab as={Fragment}>
-          {({ hover, selected }) => (
-            <button className={clsx(hover && 'underline', selected && 'bg-blue-500 text-white')}>
-              Tab 3
-            </button>
-          )}
-        </Tab>
+    <TabGroup selectedIndex={tabIndex} onChange={setTabIndex}>
+      <TabList className={`flex w-max ${getTabListCn}`}>
+        {tabs.map((tab) => (
+          <Tab as={Fragment} key={tab.key}>
+            {({ selected }) => (
+              <button
+                className={cn(
+                  buttonTabStyles({
+                    variant: selected ? `${variantTab}Selected` : variantTab,
+                    className,
+                  }),
+                )}
+                {...props}
+              >
+                {tab.name}
+              </button>
+            )}
+          </Tab>
+        ))}
       </TabList>
-      <TabPanels>
-        <TabPanel>Content 1</TabPanel>
-        <TabPanel>Content 2</TabPanel>
-        <TabPanel>Content 3</TabPanel>
-      </TabPanels>
+      <TabPanels className="mt-2 px-4 py-2">{tabs[tabIndex].children}</TabPanels>
     </TabGroup>
   );
 };
