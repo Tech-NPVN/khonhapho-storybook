b024bb2f1b2eaa8a220dbf4e86f05376e21418d7
bou2201 <lamchuc123456@gmail.com>
2024-05-10 11:32:37 +0700

    feat(components): :zap: add form handler for `Checkbox` component

---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+import { Controls, Description, Primary, Stories, Subtitle, Title } from '@storybook/blocks';
+import type { Preview } from '@storybook/react';
+import React from 'react';
+import '../src/index.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+          <Stories />
+        </>
+      ),
+    },
+  },
+};
+
+export default preview;
