5d361a1fbd3c158ae158fced7b14a1786116d748
KiAnh2911 <matic2kaa@gmail.com>
2024-05-15 14:36:09 +0700

    fix(pages): :bug: fix `layout`

--- src/pages/Layout/AuthLayout.tsx
+++ src/pages/Layout/AuthLayout.tsx
@@ -7,16 +7,10 @@ export const AuthLayout = () => {
   const [openNavBarMobile, setOpenNavBarMobile] = useState<boolean>(false);
   const { theme, toggleTheme } = useMode();
   const location = useLocation();
-  console.log('🚀 ~ AuthLayout ~ location:', location);
 
   return (
     <>
       <Header setOpenNavBarMobile={setOpenNavBarMobile} theme={theme as string} />
-      {/* {location.pathname === '/message' ? (
-        <main className="lg:mt-[63px] mt-0 bg-white dark:bg-secondaryColorDarkD2 h-[calc(100vh_-_63px)]">
-          <Outlet />
-        </main>
-      ) : ( */}
       <div className="lg:flex block lg:mt-[63px] mt-0">
         <Sidebar
           openNavBarMobile={openNavBarMobile}
@@ -25,7 +19,7 @@ export const AuthLayout = () => {
           toggleTheme={toggleTheme}
         />
         <main
-          className={`lg:w-[calc(100vw_-_250px)] relative w-screen mx-auto px-3 pt-6 lg:p-4 ml-0 lg:ml-[250px] bg-secondaryColorLightD2 dark:bg-secondaryColorDarkD2 ${location.pathname === '/message' ? 'lg:w-full lg:ml-0' : 'lg:ml-[250px] lg:w-[calc(100vw_-_250px)]'}`}
+          className={`relative w-screen mx-auto bg-secondaryColorLightD2 dark:bg-secondaryColorDarkD2 ${location.pathname === '/message' ? 'lg:w-full lg:ml-0' : 'lg:ml-[250px] lg:w-[calc(100vw_-_250px)] ml-0 px-3 pt-6 lg:p-4'}`}
         >
           <Outlet />
         </main>
